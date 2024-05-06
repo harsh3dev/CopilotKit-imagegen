c75917de4d22b02b80b43771794f34529cc232c5
Harsh Pandey <pandey73harsh@gmail.com>
2024-05-07 01:21:46 +0530

    implemented local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import openai from "openai";
 import OpenAI from "openai";
 import "./App.css";
@@ -11,7 +11,13 @@ function App() {
   const [placeholder, setPlaceholder] = useState(
     "Enter your prompt"
   );
-
+  useEffect(() => {
+    const items = localStorage.getItem('img');
+    if (items) {
+      setResult(items);
+      console.log("getitem", items);
+    }
+  }, []);
   const generateImage = async () => {
     setPlaceholder(`Search ${prompt}..`);
     setLoading(true);
@@ -27,19 +33,31 @@ function App() {
         n: 1,
         size: "256x256",
       });
-      console.log(res.data);
+      console.log(res.data[0].url);
       setResult(res.data[0].url);
+      localStorage.setItem('img', res.data[0].url);
     } catch (error) {
       console.error("Error generating image:", error);
-      setResult("");
     } finally {
       setLoading(false);
+      setPrompt("");
     }
   };
+
+
+  // useEffect(() => {
+  //   localStorage.setItem('img', JSON.stringify(result));
+  //   console.log("setitem", result);
+  // }, [result]);
+
+  
   return (
     <div className="w-screen min-h-screen text-white flex items-center justify-between ">
-      <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-      <div className="w-1/2 flex flex-col justify-center items-center gap-4">
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+
+      {/* <div className="w-1/2 flex flex-col justify-center items-baseline gap-4"> */}
+      <div className="w-1/2 flex flex-col justify-center items-baseline md:p-20 gap-4" >
+      {/* <form onSubmit={generateImage} className="w-full flex flex-col justify-center items-baseline md:p-20 gap-4" > */}
         <h2>Generate an Image using Open AI API</h2>
 
         <textarea
@@ -48,12 +66,14 @@ function App() {
           onChange={(e) => setPrompt(e.target.value)}
           rows="4"
           cols="4"
-        />
-        <button onClick={generateImage}>Generate an Image</button>
+          required
+          />
+        <button onClick={generateImage} >Generate an Image</button>
+          {/* </form> */}
       </div>
 
       <div className=" w-1/2 min-h-screen border-l-2 border-gray-500 flex flex-col justify-center items-center gap-5 ">
-            { !prompt && <FaImage />}
+            { !result && <FaImage />}
             {loading && (<>
             <h2>Generating Image...</h2>
             <div className="lds-ripple">
@@ -65,11 +85,11 @@ function App() {
         }
           {result.length > 0 ? (<div className="min-w-[80%] flex flex-col justify-center items-center gap-10 rounded-lg p-10 ">
           <img className="result-image shadow-lg rounded-lg" src={result} alt="result" />
-            <a href={result} download="image.png" className="text-white" >
           <button>
+            <a href={result} download="image.jpeg" type="download" className="text-white" >
             Download Image
-            </button>
             </a>
+            </button>
           </div>
         ):<></>}
 
